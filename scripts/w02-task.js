dd460ceb8194bc5ddeeb7a0255aabc2734aaafd5
julioaraujo139 <jaraujo.byupathway@outlook.com>
2024-01-21 00:55:13 -0300

    changes

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -1,39 +1,22 @@
 /* W02-Task - Profile Home Page */
-const fullName = 'Your Name';
-const currentYear = 'new Date().getFullyear()';
-const ProfilePicture = 'images/personal_photo.jpg';
-
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
+let fullName = 'Julio Araujo';
+let currentYear = '2024';
+let profilePicture = 'images/profile-image.jpg';
+
+/* Step 3 - Element Variables */
 const nameElement = document.getElementById('name');
 const foodElement = document.getElementById('food');
 const yearElement = document.querySelector('#year');
-const imageElement = document.querySelector('#profile-image');
-
-/* Step 3 - Element Variables */
-
-
-
-
+const imageElement = document.querySelector('#image');
 
 /* Step 4 - Adding Content */
-// Set nameElement's innerHTML property with fullName variable value
 nameElement.innerHTML = `<strong>${fullName}</strong>`;
-
-// Use textContent property to set the value of yearElement to currentYear variable value
-yearElement.textContent = currentYear;
-
-// Use setAttribute method to set the src property of imageElement
-imageElement.setAttribute('src', profilePicture);
-
-// Use setAttribute method to set the alt property of imageElement
-imageElement.setAttribute('alt', `Profile image of ${fullName}`);
-
-
-
-
-
+yearElement.innerHTML = `${currentYear}`;
+imageElement.setAttribute('src' , profilePicture);
+imageElement.setAttribute('alt' , `Profile image of ${fullName}`);
 
 /* Step 5 - Array */
 
