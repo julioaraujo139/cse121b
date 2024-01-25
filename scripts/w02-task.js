0bc38cfb0b6f570ed811cc649bf3d608fd1fdeab
julioaraujo139 <jaraujo.byupathway@outlook.com>
2024-01-24 22:30:44 -0300

    changes

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -4,24 +4,22 @@
 /* Step 2 - Variables */
 let fullName = 'Julio Araujo';
 let currentYear = '2024';
-let profilePicture = 'images/profile-image.jpg';
+let profilePicture = 'images/personal-photo.png';
+let favFoods = ['Rice', 'bean', 'Spaghetti', 'Steak'];
+favFoods.push('Salad', 'Banana Cream Pie');
+console.log(favFoods);
 
 /* Step 3 - Element Variables */
 const nameElement = document.getElementById('name');
 const foodElement = document.getElementById('food');
 const yearElement = document.querySelector('#year');
-const imageElement = document.querySelector('#image');
+const imageElement = document.querySelector('img');
 
 /* Step 4 - Adding Content */
 nameElement.innerHTML = `<strong>${fullName}</strong>`;
 yearElement.innerHTML = `${currentYear}`;
+foodElement.innerHTML = `<br>${favFoods}`;
 imageElement.setAttribute('src' , profilePicture);
 imageElement.setAttribute('alt' , `Profile image of ${fullName}`);
 
 /* Step 5 - Array */
-
-
-
-
-
-
