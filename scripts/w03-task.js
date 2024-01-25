c09d16ee115edfb28776380cadc9e627fc741d55
julioaraujo139 <jaraujo.byupathway@outlook.com>
2024-01-25 00:50:36 -0300

    change

---
+++ scripts/w03-task.js
@@ -0,0 +1,82 @@
+/* LESSON 3 - Programming Tasks */
+
+/* FUNCTIONS */
+/* Function Definition - Add Numbers */
+function add (number1, number2) {
+    return number1 + number2;
+}
+function addNumbers() {
+    let addNumber1 = Number(document.querySelector('#add1').value);
+    let addNumber2 = Number(document.querySelector('#add2').value);
+    document.querySelector('#sum').value = add(addNumber1, addNumber2);
+}
+
+document.querySelector('#addNumbers').addEventListener('click', addNumbers);
+
+/* Function Expression - Subtract Numbers */
+function subtract (number1, number2) {
+    return number1 - number2;
+}
+
+function subtractNumbers() {
+    let addNumber1 = Number(document.querySelector('#subtract1').value);
+    let addNumber2 = Number(document.querySelector('#subtract2').value);
+    document.querySelector('#difference').value = subtract(addNumber1, addNumber2);
+}
+
+document.querySelector('#subtractNumbers').addEventListener('click', subtractNumbers);
+/* Arrow Function - Multiply Numbers */
+const multiply = (number1, number2) => number1 * number2;
+
+const multiplyNumbers = () => {
+  let multiplyNumber1 = Number(document.querySelector('#factor1').value);
+  let multiplyNumber2 = Number(document.querySelector('#factor2').value);
+  document.querySelector('#product').value = multiply(multiplyNumber1, multiplyNumber2);
+}
+
+document.querySelector('#multiplyNumbers').addEventListener('click', multiplyNumbers);
+
+/* Open Function Use - Divide Numbers */
+function divide(number1, number2) {
+    return number1 / number2;
+}
+  
+function divideNumbers() {
+    let divideNumber1 = Number(document.querySelector('#dividend').value);
+    let divideNumber2 = Number(document.querySelector('#divisor').value);
+    document.querySelector('#quotient').value = divide(divideNumber1, divideNumber2);
+}
+  
+document.querySelector('#divideNumbers').addEventListener('click', divideNumbers);
+  
+/* Decision Structure */
+document.querySelector('#getTotal').addEventListener('click', function() {
+    let subtotal = Number(document.querySelector('#subtotal').value);
+    
+    if (document.querySelector('#member').checked) {
+        subtotal *=0.8;
+    }
+
+    document.querySelector('#total').textContent = `Total: $${subtotal.toFixed(2)}`;
+});
+
+/* ARRAY METHODS - Functional Programming */
+/* Output Source Array */
+let numbersArray = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+
+document.querySelector('#array').innerHTML = numbersArray.filter(number => number);
+
+/* Output Odds Only Array */
+document.querySelector('#odds').innerHTML = numbersArray.filter(number => number % 2 === 1);
+
+/* Output Evens Only Array */
+document.querySelector('#evens').innerHTML = numbersArray.filter(number => number % 2 === 0);
+
+/* Output Sum of Org. Array */
+document.querySelector('#sumOfArray').innerHTML = numbersArray.reduce((sum, number) => sum + number, 0);
+
+/* Output Multiplied by 2 Array */
+document.querySelector('#multiplied').innerHTML = numbersArray.map(number => number * 2);
+
+/* Output Sum of Multiplied by 2 Array */
+document.querySelector('#sumOfMultiplied').innerHTML = numbersArray.map(number => number * 2).reduce((sum, number) => sum + number, 0);
