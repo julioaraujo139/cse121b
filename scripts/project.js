2b8d860dabecf5e94ccdfe7981d1c91a38fe100b
julioaraujo139 <jaraujo.byupathway@outlook.com>
2024-02-14 23:50:51 -0300

    change

--- scripts/project.js
+++ scripts/project.js
@@ -1,69 +1,47 @@
 /* W05: Programming Tasks */
 
 /* Declare and initialize global variables */
-const templesElement = document.querySelector("#temples");
-const templeList = [];
+const apostlesElement = document.querySelector("#apostles");
+const apostleList = [];
 
 /* async displayTemples Function */
-const displayTemples = (temples) => {
-    temples.forEach((temple) => {
+const displayApostles = (apostles) => {
+    apostles.forEach((apostle) => {
       const article = document.createElement("article");
       
-      const h3 = document.createElement("h3");
-      h3.textContent = temple.ApostleName;
+      const ApostleName = document.createElement("h3");
+      ApostleName.textContent = apostle.ApostleName;
 
-      const h4 = document.createElement("h4");
-      h4.textContent = temple.birthDate;
+      const birthDate = document.createElement("h4");
+      birthDate.textContent = apostle.birthDate;
+
+      const BirthLocation = document.createElement("h4");
+      BirthLocation.textContent = apostle.BirthLocation;
+
+      const CallDate = document.createElement("h4");
+      CallDate.textContent = apostle.CallDate;
       
       const img = document.createElement("img");
-      img.src = temple.imageUrl;
-      img.alt = temple.location;
-  
-      article.appendChild(h3);
-      article.appendChild(h4);
+      img.src = apostle.imageUrl;
+      img.alt = apostle.location;
+      
       article.appendChild(img);
+      article.appendChild(ApostleName);
+      article.appendChild(birthDate);
+      article.appendChild(BirthLocation);
+      article.appendChild(CallDate);
+      
   
-      templesElement.appendChild(article);
+      apostlesElement.appendChild(article);
     });
   };
 
 /* async getTemples Function using fetch()*/
-const getTemples = async () => {
+const getApostles = async () => {
     const response = await fetch("https://run.mocky.io/v3/08c2e4ca-a7cd-48e3-bad8-851225bd6e5b");
     const data = await response.json();
-    templeList.push(...data);
-    displayTemples(templeList);
+    apostleList.push(...data);
+    displayApostles(apostleList);
     };
-    
-/* reset Function */
-const reset = () => {
-    templesElement.innerHTML = "";
-  };
 
-/* filterTemples Function */
-const filterTemples = (temples) => {
-    reset();
-  
-    const filter = document.querySelector("#filtered").value;
-  
-    switch (filter) {
-      case "utah":
-        displayTemples(temples.filter(temple => temple.location.includes("Utah")));
-        break;
-      case "notutah":
-        displayTemples(temples.filter(temple => !temple.location.includes("Utah")));
-        break;
-      case "older":
-        displayTemples(temples.filter(temple => new Date(temple.dedicated) < new Date(1950, 0, 1)));
-        break;
-      case "all":
-      default:
-        displayTemples(temples);
-        break;
-    }
-  };
-
-/* Event Listener */
-document.querySelector("#filtered").addEventListener("change", () => { filterTemples(templeList) });
-
-getTemples();
+getApostles();
